d2965fa59f48527a83a94ac52fa07a32621af17d
magent <magent@cryptograss.live>
2026-02-26 15:36:28 +0000

    Add special page aliases to fix MediaWiki notices

---
+++ extensions/PickiPediaInvitations/PickiPediaInvitations.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for special pages of the PickiPediaInvitations extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'CreateInvite' => [ 'CreateInvite', 'Create Invite' ],
+	'ManageInvites' => [ 'ManageInvites', 'Manage Invites' ],
+];
